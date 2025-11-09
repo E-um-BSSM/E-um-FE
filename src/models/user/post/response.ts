845f63c951901102b5da7c1cd0c4480fbe12a342
shusbox <shusbox@naver.com>
2025-11-09 22:26:40 +0900

    feat: postCreate 도메인 요청 / 응답 인터페이스 추가

---
+++ src/models/user/post/response.ts
@@ -0,0 +1,10 @@
+export interface postCreateResponse {
+  post_id: number;
+  author_id: string;
+  title: string;
+  body: string;
+  tags: string[];
+  draft: boolean;
+  create_at: string;
+  updated_at: string;
+}
