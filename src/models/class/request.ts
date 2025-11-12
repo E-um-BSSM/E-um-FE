dd037839804dd41461c0a846bc7975fd95df50a8
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-11-12 12:37:02 +0900

    feat: class 요청,응답 interface 추가

---
+++ src/models/class/request.ts
@@ -0,0 +1,37 @@
+export interface classRequest{
+ title:string;
+ description:string;
+ difficulty:string;
+ tags : string[];
+ status : string;
+}
+
+export interface classSearchRequest{
+ difficulty:string;
+ tags:string[];
+ status : string;
+}
+
+export interface assignmentSubmitRequest{
+ content : string;
+ file_url: string;
+}
+
+export interface assignmentRequest{
+ title:string;
+ description:string;
+ due_date:string;
+ difficulty:string;
+}
+
+export interface classNotificationRequest{
+ title: string;
+ content: string
+}
+
+export interface classScheduleRequset{
+ title:string;
+ description:string;
+ start_at:string;
+ end_at:string;
+}
