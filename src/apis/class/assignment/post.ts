fc8b6935c593289a44e8221d25765863bc54438b
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-11-19 00:05:51 +0900

    feat: assignment api 함수 수정

--- src/apis/class/assignment/post.ts
+++ src/apis/class/assignment/post.ts
@@ -1,24 +1,13 @@
 import { req } from "@/apis/axiosInstance";
 import type {
   assignmentRequest,
-  assignmentSubmitRequest,
   assignmentResponse,
-  assignmentSubmitResponse,
   globalResponse,
 } from "@/models/index";
 
 export const assignmentPOST = {
   assignmentCreate: async (class_id: number, body: assignmentRequest): Promise<globalResponse<assignmentResponse>> => {
     const response = await req.post(`/classes/${class_id}/assignments`, body);
-    return response.data;
-  },
-
-  assignmentSubmit: async (
-    class_id: number,
-    assignment_id: number,
-    body: assignmentSubmitRequest,
-  ): Promise<globalResponse<assignmentSubmitResponse>> => {
-    const response = await req.post(`/classes/${class_id}/${assignment_id}`, body);
     return response.data;
   },
 };
