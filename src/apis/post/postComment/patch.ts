1f1119002cabc6cbf80b864c44813a3cd250c992
shusbox <shusbox@naver.com>
2025-11-19 15:45:07 +0900

    refactor: prettier 실행

--- src/apis/post/postComment/patch.ts
+++ src/apis/post/postComment/patch.ts
@@ -1,13 +1,25 @@
 import { req } from "@/apis/axiosInstance";
-import type { postCommentRequest, postCommentAdoptRequest, postCommentResponse, postCommentCreateResponse, globalResponse } from "@/models";
+import type {
+  postCommentRequest,
+  postCommentAdoptRequest,
+  postCommentResponse,
+  postCommentCreateResponse,
+  globalResponse,
+} from "@/models";
 
 export const postCommentPATCH = {
-  commentUpdate: async (comments_id: number, body: postCommentRequest): Promise<globalResponse<postCommentCreateResponse>> => {
+  commentUpdate: async (
+    comments_id: number,
+    body: postCommentRequest,
+  ): Promise<globalResponse<postCommentCreateResponse>> => {
     const response = await req.patch(`/posts/comments/${comments_id}`, body);
     return response.data;
   },
 
-  commentAdopt: async (comments_id: number, body: postCommentAdoptRequest): Promise<globalResponse<postCommentResponse>> => {
+  commentAdopt: async (
+    comments_id: number,
+    body: postCommentAdoptRequest,
+  ): Promise<globalResponse<postCommentResponse>> => {
     const response = await req.patch(`/posts/comments/${comments_id}/adopt`, body);
     return response.data;
   },
