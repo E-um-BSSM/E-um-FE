ced9cb345946580f049ed3950e0aa15899ed6860
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-11-20 01:14:43 +0900

    feat: header 달아주는 코드 작성

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -4,3 +4,15 @@ export const req = axios.create({
   baseURL: import.meta.env.API_URL,
   withCredentials: true,
 });
+
+req.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  
+  if (config.data && !(config.data instanceof FormData)) {
+    config.headers["Content-Type"] = "application/json";
+  }
+  
+  return config;
+});
