9e2c8751c22530eea58ab0162a62f5069283f402
ketarubot <82302028+ketarubot@users.noreply.github.com>
2025-11-23 13:56:19 +0900

    Feat#8 (#9)
    * feat: api연결 기초 설정
    
    * feat: 폴더별 index.ts 추가
    
    * feat: user 도메인 요청/응답 인터페이스 추가
    
    * chore: src alias 문제 해결
    
    * chore: emotion/styled 추가
    
    * feat: css 변수 등록
    
    * feat: css 변수 등록
    
    * feat: Pretendard 폰트 추가
    
    * chore: logo asset 추가
    
    * chore: insta, github  asset 추가
    
    * chore: tokens, fonts css 파일 분리
    
    * feat: 전역 스타일 css 파일 추가
    
    * feat: 전역 스타일 파일 추가
    
    * fix: spacing 변수값 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
+import "./tokens.css";
+import "./fonts.css";
+import "./global.css";
 
 const queryClient = new QueryClient();
 
