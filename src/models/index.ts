828ffd20b26ebb2551adf8e143fcd669b2ac9859
shusbox <shusbox@naver.com>
2025-11-23 14:01:32 +0900

    feat: transaction 스카미 추가

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 export * from "./user";
 export * from "./class";
 export * from "./global/response";
 export * from "./post";
 export * from "./push";
-=======
-export * from "./global/response";
-export * from "./post";
-export * from "./push";
-export * from "./user";
->>>>>>> f2c1334 (feat: post api 함수 추가)
