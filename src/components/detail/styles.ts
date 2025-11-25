b7099a8ab252f7130821545ad4e97751def559d5
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-11-25 17:43:10 +0900

    feat:detail 컴포넌트 style 수정

--- src/components/detail/styles.ts
+++ src/components/detail/styles.ts
@@ -1,4 +1,3 @@
-import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
 export const Base = styled.div`
@@ -13,17 +12,20 @@ export const Base = styled.div`
   font-size: 1.125rem;
   font-style: normal;
   font-weight: 500;
-  line-height: 1.375rem; /* 122.222% */
-`;
+  line-height: 1.375rem;
 
-export const Status = {
-  deactivate: css({
-    borderRadius: "var(--XS, 0.5rem)",
-    border: "1px solid var(--Natural-natural-200, #E7EBEE)",
-    background: "var(--white, #FFF)",
-  }),
-  activate: css({
-    borderRadius: "var(--XS, 0.5rem)",
-    background: "var(--Natural-natural-200, #E7EBEE)",
-  }),
-};
+  border-radius: var(--XS, 0.5rem);
+  border: 1px solid var(--Natural-natural-200, #E7EBEE);
+  background: var(--white, #FFF);
+  transition: 0.3s;
+
+  &:hover {
+    background: var(--Natural-natural-100, #F3F6F8);
+    cursor: pointer;
+    transition: 0.3s;
+  }
+
+  &:active {
+    background: var(--Natural-natural-200, #E7EBEE);
+  }
+`;
