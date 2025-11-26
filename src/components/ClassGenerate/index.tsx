8b8b11c25d753b3a147109cc6dfbb8cbc06b9d94
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-11-26 14:05:48 +0900

    refactor: 가독성

--- src/components/ClassGenerate/index.tsx
+++ src/components/ClassGenerate/index.tsx
@@ -5,8 +5,6 @@ import { Base } from "@/components/ClassGenerate/style";
 export default function ClassGenerate() {
   return (
     <Base>
-      <img src={Class} alt="Docs"/>
-      클래스 개설하기
-    </Base>
+      <img src={Class} alt="Docs"/>클래스 개설하기</Base>
   );
 }
